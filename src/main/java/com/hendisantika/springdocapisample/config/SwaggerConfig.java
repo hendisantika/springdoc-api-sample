92e272fc993fc370843fffb4037fa41401cc0ebd
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-25 06:57:01 +0700

    #20 - Add customOpenAPI in SwaggerConfig

--- src/main/java/com/hendisantika/springdocapisample/config/SwaggerConfig.java
+++ src/main/java/com/hendisantika/springdocapisample/config/SwaggerConfig.java
@@ -1,6 +1,10 @@
 package com.hendisantika.springdocapisample.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,5 +26,18 @@ public class SwaggerConfig {
                 .group("demo")
                 .pathsToMatch("/**")
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDescription, @Value("${application" +
+            "-version}") String appVersion) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Doc Application API")
+                        .version(appVersion)
+                        .description(appDescription)
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
     }
 }
