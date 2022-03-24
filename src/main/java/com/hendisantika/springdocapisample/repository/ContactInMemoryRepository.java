4505a138fe5732dd4bf2e0e14b3dbd67a0ed97e9
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-25 06:37:01 +0700

    #7 - Init data in ContactInMemoryRepository

--- src/main/java/com/hendisantika/springdocapisample/repository/ContactInMemoryRepository.java
+++ src/main/java/com/hendisantika/springdocapisample/repository/ContactInMemoryRepository.java
@@ -20,4 +20,13 @@ import java.util.List;
 public class ContactInMemoryRepository implements ContactRepository {
 
     private static final List<Contact> contacts = new ArrayList<>();
+
+    public ContactInMemoryRepository() {
+        // initialize test data
+        contacts.add(new Contact(1L, "Uzumaki", "Naruto", "uzumaki_naruto@konohagakure.co.jp", "123-456-789", "address 123, street 1, city 1"));
+        contacts.add(new Contact(2L, "Uchiha", "Sasuke", "uchiha_sasuke@konohagakure.co.jp", "123-456-788", "address 456, street 2, city 1"));
+        contacts.add(new Contact(3L, "Haruno", "Sakura", "haruno_sakura@konohagakure.co.jp", "123-456-787", "address 789, street 3, city 1"));
+        contacts.add(new Contact(4L, "Hatake", "Kakashi", "hatake_kakashi@konohagakure.co.jp", "123-456-786", "address 234, street 4, city 1"));
+        contacts.add(new Contact(5L, "Rock", "Lee", "rock_lee@konohagakure.co.jp", "123-456-785", "address 678, street 5, city 1"));
+    }
 }
