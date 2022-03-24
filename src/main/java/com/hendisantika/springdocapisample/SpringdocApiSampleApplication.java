f5f715b634f7fd6b0f5d3619bd5307d15107cc30
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-25 06:21:24 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springdocapisample/SpringdocApiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springdocapisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocApiSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdocApiSampleApplication.class, args);
+    }
+
+}
